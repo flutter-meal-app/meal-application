9292a93d60eeb3414d7e30f7c07401c0132af8b7
mune-Geeks <yoshidamnhr@gmail.com>
2023-12-22 15:19:17 +0900

    基本画面３ページ用意

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'main.dart';
 
 class SplashScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
+    
+    Timer(
+      Duration(seconds: 1), 
+      () {
+        // Navigatorを使ってページの遷移を行う
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => MyHomePage(title: 'Flutter Demo',),
+          ),
+        );
+      },
+    );
+    
     return Scaffold(
       backgroundColor: Colors.yellow, // 背景色を黄色に設定
       body: Center(
@@ -18,3 +34,4 @@ class SplashScreen extends StatelessWidget {
     );
   }
 }
+
